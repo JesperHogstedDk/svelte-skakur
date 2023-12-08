1fc862bd946ce1a8f0f40ee2a8d0b6419b013d51
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2023-12-08 12:24:25 +0100

    Attempt to deploy with gibhub

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
+<script>
+	// This can be false if you're using a fallback (i.e. SPA mode)
+	export const prerender = true;
+</script>
+
 <nav>
 	<a href="/">Home</a>
 	<a href="/skak">Skakur</a>
-    <!-- <a href="/blog">Blog</a> -->
+	<!-- <a href="/blog">Blog</a> -->
 	<a href="/om">Om uret</a>
 </nav>
 
-<slot/>
+<slot />
